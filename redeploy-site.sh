8456670f187c3fe99ea8d53101879bc8ed1667e5
sgabrani <66939732+sgabrani@users.noreply.github.com>
2023-07-30 02:53:56 +0530

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,25 +1,13 @@
 #!/bin/bash
-cd /root/Portfolio
-git fetch && git reset origin/main --hard
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
-cd /etc/systemd/system
-systemctl daemon-reload
-systemctl restart myportfolio
-<<comment
-# Step 1: Kill all existing tmux sessions
-tmux kill-server
 
-# Step 2: Change directory to your project folder
-cd Portfolio/
+# Step 1: Change directory to your project folder
+cd /path/to/your/project/folder
 
-# Step 3: Fetch the latest changes from GitHub and reset the local repository
+# Step 2: Fetch the latest changes from the main branch on GitHub
 git fetch && git reset origin/main --hard
 
-# Step 4: Enter the Python virtual environment and install dependencies
-source python3-virtualenv/bin/activate
-pip install -r requirements.txt
+# Step 3: Spin containers down to prevent out of memory issues
+docker-compose -f docker-compose.prod.yml down
 
-# Step 5: Start a new detached Tmux session and run the Flask server
-tmux new-session -d 'flask run --host=0.0.0.0'
-comment
+# Step 4: Build and bring up the containers
+docker-compose -f docker-compose.prod.yml up -d --build
