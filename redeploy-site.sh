a87cccf880f2f25650247d68e4a21b483c5e964f
sgabrani <66939732+sgabrani@users.noreply.github.com>
2023-08-02 17:51:45 +0530

    Update redeploy-site.sh

--- redeploy-site.sh
+++ redeploy-site.sh
@@ -1,13 +1,5 @@
 #!/bin/bash
-
-# Step 1: Change directory to your project folder
 cd /root/Portfolio
-
-# Step 2: Fetch the latest changes from the main branch on GitHub
 git fetch && git reset origin/main --hard
-
-# Step 3: Spin containers down to prevent out of memory issues
 docker-compose -f docker-compose.prod.yml down
-
-# Step 4: Build and bring up the containers
 docker-compose -f docker-compose.prod.yml up -d --build
