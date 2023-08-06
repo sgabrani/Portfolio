f2f1460776267ab9a93827bf4dd8ea56ccb9dd17
sgabrani <66939732+sgabrani@users.noreply.github.com>
2023-08-06 18:32:13 +0530

    Update information.py

--- app/information.py
+++ app/information.py
@@ -1,10 +1,10 @@
 info = {
     # Your first and last name
-    'name' : 'NAME',
+    'name' : 'Sahiti Gabrani',
     # Where you live / are from
-    'location' : "Planet Earth",
+    'location' : "Los Angeles",
     # The name of your university
-    'university' : 'University Name',
+    'university' : 'University of California, Los Angeles',
     # Information about you
     'about_yourself' : "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
     'about_yourself2' : "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit",
