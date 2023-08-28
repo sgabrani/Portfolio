b496f6134ad69d981eae966e006f694879628c7b
Sahiti Gabrani <sahitigabrani@gmail.com>
2023-08-28 21:00:20 +0530

    change path in run test sh

--- run_test.sh
+++ run_test.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-$PWD/python3-virtualenv/Scripts/python.exe -m unittest discover -v tests/
+C:/Users/sahit/Portfolio/python3-virtualenv/Scripts/python.exe -m unittest discover -v tests/
